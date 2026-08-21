5cf4d9801321a25c4305e2a4d3cfbb0554040e0c
Akil Maharjan <akilmaharjan44@gmail.com>
2026-08-21 10:30:29 +0545

    array taught

---
+++ array.js
@@ -0,0 +1,4 @@
+let num = [1, 2 ,3,4 ,5];
+
+const sumOfAllNUmbers = num.reduce((previous, current)=> previous * current ,1);
+console.log(sumOfAllNUmbers);
---
+++ script.js
@@ -0,0 +1,48 @@
+//   let  marks = 90;
+  
+//   let isStudent
+//   console.log(isStudent ? "you are a student" : "you are not a student");
+
+// console.log(marks >=90 ? "A+" : marks >=80 ? "A" : marks >=70 ? "B" : marks >=60 ? "C" : marks >=50 ? "D" : "F");
+
+
+//  for(let i = 10; i >=0; i--){
+//    console.log(i);
+//  }
+
+  
+//   function arithmetic(a, b, operator){
+//     switch(operator){
+//       case "+":
+//         return a + b;
+//       case "-":
+//         return a - b;
+//       case "*":
+//         return a * b;
+//       case "/":
+//         return a / b;
+//       default:
+//         return "Invalid operator";
+//     }
+//   }
+
+
+//   console.log(arithmetic(2, 3, "+"));
+//   console.log(arithmetic(2, 3, "-"));
+//   console.log(arithmetic(2, 3, "*"));
+//   console.log(arithmetic(2, 3, "/"));
+
+
+//   const a = (a ,b)=> {
+//     return a + b;
+//   }
+
+//   console.log(a(5 , 5));
+
+// function isEven(num){
+//    return num % 2 === 0 ? `${num} is even` : `${num} is not even`
+// }
+// console.log(isEven(7))
+
+
+
