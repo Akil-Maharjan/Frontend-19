8d53886324e0cf60cdb51601c27b5cceb6a9aed4
Akil Maharjan <akilmaharjan44@gmail.com>
2026-08-23 15:55:03 +0545

    revision

--- script.js
+++ script.js
@@ -1,48 +1,161 @@
-//   let  marks = 90;
-  
-//   let isStudent
-//   console.log(isStudent ? "you are a student" : "you are not a student");
+//    string method
+// let str= "helloh World";
 
-// console.log(marks >=90 ? "A+" : marks >=80 ? "A" : marks >=70 ? "B" : marks >=60 ? "C" : marks >=50 ? "D" : "F");
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.charAt(5));
+// console.log(str.indexOf(" "));
+// console.log(str.includes("h"))
+// console.log(str.lastIndexOf("h"));
+// console.log(str.replaceAll("h", "H"));
+// console.log(str.slice(-5));
+
+//   number Method
+
+//   let num = 4.9;
+//   console.log(Math.floor(num));
+//   console.log(Math.ceil(num));
+//   console.log(Math.round(num));
+//   console.log(Math.abs(num));
+// console.log(Math.floor(Math.random() *10 +1))
+// console.log(Math.min(2, 50, 10));
+// console.log(Math.pow(2, 3));
+
+//  operators
+
+//  let a =false;
+//  console.log(a ?? "A has no value")
+ 
+//  console.log(a -b);
+//  console.log(a *b);
+//  console.log(a /b);
+//  console.log(a %b);
+//  console.log(a **b);
 
 
-//  for(let i = 10; i >=0; i--){
-//    console.log(i);
-//  }
+// conditions
 
-  
-//   function arithmetic(a, b, operator){
-//     switch(operator){
-//       case "+":
-//         return a + b;
-//       case "-":
-//         return a - b;
-//       case "*":
-//         return a * b;
-//       case "/":
-//         return a / b;
-//       default:
-//         return "Invalid operator";
-//     }
-//   }
+// let marks = 60;
 
-
-//   console.log(arithmetic(2, 3, "+"));
-//   console.log(arithmetic(2, 3, "-"));
-//   console.log(arithmetic(2, 3, "*"));
-//   console.log(arithmetic(2, 3, "/"));
-
-
-//   const a = (a ,b)=> {
-//     return a + b;
-//   }
-
-//   console.log(a(5 , 5));
-
-// function isEven(num){
-//    return num % 2 === 0 ? `${num} is even` : `${num} is not even`
+// if(marks >= 90){
+//     console.log("A+");
 // }
-// console.log(isEven(7))
+// else if(marks >= 80){
+//     console.log("A");
+// }
+// else if(marks >= 70){
+//     console.log("B+");
+// }
+// else if (marks >= 60){
+//     console.log("B");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+//   let month = Number(prompt("Enter a month number (1-12):"));
+
+//   switch (month) {
+//     case 1:
+//       console.log("January");
+//       break;
+//     case 2:
+//       console.log("February");
+//       break;
+//     case 3:
+//       console.log("March");
+//       break;
+//     case 4:
+//       console.log("April");
+//       break;
+//     case 5:
+//       console.log("May");
+//       break;
+//     case 6:
+//       console.log("June");
+//       break;
+//     case 7:
+//       console.log("July");
+//       break;
+//     case 8:
+//       console.log("August");
+//       break;
+//     case 9:
+//       console.log("September");
+//       break;
+//     case 10:
+//       console.log("October");
+//       break;
+//     case 11:
+//       console.log("November");
+//       break;
+//     case 12:
+//       console.log("December");
+//       break;
+//     default:
+//       console.log("Invalid month");
+//       break;
+//   }
+
+// let marks = 90;
+// marks >= 90
+//   ? console.log("A+")
+//   : marks >= 80
+//     ? console.log("A")
+//     : marks >= 70
+//       ? console.log("B+")
+//       : marks >= 60
+//         ? console.log("B")
+//         : console.log("Fail");
 
 
+//    Loops
+// for(let i =1; i <10; i++){
+//     for(j= 1; j < 10; j++){
+//  console.log(i, j);
+//     }
+   
+// }
 
+
+// let i = 1;
+// while (i >= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// do {
+//     console.log(i);
+// } while (i >=10);
+
+
+// function
+//    greet("Akil");
+//   function greet(name){
+//     console.log("Hello" + " "+ name);
+//    }
+  
+//    const a =(name)=>{
+//     return ("Hello" + " " + name);
+   
+//    }
+//    console.log(a("Akil"));
+
+function Operations(a,b, operator){
+    switch(operator)
+    {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
+        default:
+            return "Invalid operator";
+    }
+}
+
+console.log(Operations(2, 0, "/"))
