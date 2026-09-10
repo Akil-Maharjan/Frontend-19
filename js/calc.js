4f39f0a716e6ab4698c15ae84a884c73309d8a82
Akil Maharjan <akilmaharjan44@gmail.com>
2026-09-10 15:42:35 +0545

    reverted

---
+++ js/calc.js
@@ -0,0 +1,17 @@
+
+
+export function addNUmber(a ,b){
+   console.log(`Added NUmber is ${a + b}`);
+}
+
+export function subNUmber(a ,b){
+   console.log(`Subtracted NUmber is ${a - b}`);
+}
+
+export function multiply(a, b){
+    console.log(`Multiplied NUmber is ${a * b}`);
+}
+
+export function division(a, b){
+    console.log(`Divided number is ${a /b}`)
+}
